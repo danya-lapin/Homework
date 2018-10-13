25e47fa307a4f1c70a5f472ebcb4141115a366b5
danya-lapin <43727241+danya-lapin@users.noreply.github.com>
2018-10-13 17:12:02 +0300

    Add files via upload

---
+++ Gnome sort/Gnome_sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void gnome_sort(int array[], int sz) {
+    int b;
+    for (int a = 1; a < sz;) {
+        if (array[a - 1] <= array[a]) {
+            a++;
+        } else {
+            b = array[a];
+            array[a] = array[a - 1];
+            array[a - 1] = b;
+            --a;
+            if (a == 0)
+                a = 1;
+        }
+    }
+}
+int main() {
+    int array[10] = {4, 1, 8, 2, 5, 3, 11, 6, 9, 7};
+    gnome_sort(array, 10);
+    for(int i = 0; i < 10; i++) {
+        printf("%i ", array[i]);
+    }
+    return 0;
+}
---
+++ Gnome sort/cmake-build-debug/CMakeFiles/untitled4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled4.dir/Gnome_sort.c.o"
+  "untitled4.pdb"
+  "untitled4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
